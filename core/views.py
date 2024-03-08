b727c9603325a9b2c4a5fa959bd00d6975d50f45
ali <elbharya59@gmail.com>
2024-03-08 15:09:01 +0200

    initial commit

--- core/views.py
+++ core/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView , ListView
+from .models import Post
 
 # Create your views here.
-class Home(TemplateView):
+'''class Home(TemplateView):
+    template_name='home.html' '''
+
+class Home(ListView):
+    model = Post
     template_name='home.html'
+    context_object_name ='all_posts'
+
 
 class About(TemplateView):
     template_name='about.html'
